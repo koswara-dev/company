553169471831ff3317adff1ef56297fb48a9e916
Koswara <koswara.c79@gmail.com>
2025-07-08 15:07:01 +0700

    flow docker compose

--- src/main/java/com/juaracoding/company/controller/DivisionController.java
+++ src/main/java/com/juaracoding/company/controller/DivisionController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/divisions")
+@RequestMapping("/api/v1/divisions")
 public class DivisionController {
 
     @Autowired
